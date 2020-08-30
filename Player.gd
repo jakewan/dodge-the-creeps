9d3c5ce7f4580859474e806741059f65c8889746
Jacob Wan <jacobwan840@gmail.com>
2020-08-30 10:55:37 -0700

    Update Player animations

--- Player.gd
+++ Player.gd
@@ -32,9 +32,18 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-		
+
 	# Prevent the player from leaving the screen ("clamping" means restricting
 	# to a certain range).
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+	# Update the animation
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
