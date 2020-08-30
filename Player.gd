a15ef458ec65d6bde3dd4e68618b6b14f52c0da6
Jacob Wan <jacobwan840@gmail.com>
2020-08-30 08:59:55 -0700

    User can control Player

---
+++ Player.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+
+# Member variables
+
+# How fast the player will move
+export var speed = 400
+
+# Size of the game window
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Player's movement vector
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	# Prevent the player from leaving the screen ("clamping" means restricting
+	# to a certain range).
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
